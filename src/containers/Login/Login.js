b99ac7081aac55593e4fcdf8ba87a707da4eb2ca
DeweyFresco <84947283+DeweyFresco@users.noreply.github.com>
2021-05-29 11:29:29 +0800

    Add files via upload

---
+++ src/containers/Login/Login.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import './Login.css';
+
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isPasswordValid: false,
+      passwordEntered: false
+    }
+  }
+
+  passwordChangeHandler = (event) => {
+    const password = event.target.value;
+
+    this.setState({
+      passwordEntered: true
+    });
+
+    if (password.length < 5 || password.length > 10) {
+      this.setState({
+        isPasswordValid: false
+      });
+    } else {
+      this.setState({
+        isPasswordValid: true
+      });
+    }
+  }
+
+  render() {
+    let passwordBoxStyle = null;
+    if (this.state.passwordEntered && !this.state.isPasswordValid) {
+      passwordBoxStyle = {
+        boxShadow: '0 0 2px 1px rgba(255, 0, 0, 1)',
+      };
+    }
+
+    return (
+      <Container className="Login">
+        <Row>
+          <Col md={{ span: 8, offset: 2 }}>
+            <Card className="login-form-container">
+              <Form>
+                <Form.Group as={Row} controlId="formPlaintextEmail">
+                  <Form.Label column sm="2">
+                    Username
+                  </Form.Label>
+                  <Col sm="10">
+                    <Form.Control type="email" placeholder="Enter username" />
+                  </Col>
+                </Form.Group>
+
+                <Form.Group as={Row} controlId="formPlaintextPassword">
+                  <Form.Label column sm="2">
+                    Password
+                  </Form.Label>
+                  <Col sm="10">
+                    <Form.Control type="password" placeholder="Password" className="password-box" style={passwordBoxStyle} onChange={this.passwordChangeHandler} />
+                  </Col>
+                </Form.Group>
+              </Form>
+              <div>
+              <button type="submit" className="btn btn-primary" onClick="activateLasers()"> Log In </button>
+              </div>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default Login;
